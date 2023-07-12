291f12c7bdf46719f5c4d75479c5828cdb0f97fe
Patricio <pastroza@gmail.com>
2023-07-12 17:09:05 -0400

    Cambios en apollo.js

--- config/apollo.js
+++ config/apollo.js
@@ -6,8 +6,6 @@ import {setContext } from 'apollo-link-context';
 const httpLink = createHttpLink({
     //uri: ' http://localhost:4000/',
     uri: ' https://crmgraphql-c3hqmkrfsq-uc.a.run.app',
-
-    https://crmgraphql-c3hqmkrfsq-uc.a.run.app
 });
 
 //Agregamos Header
